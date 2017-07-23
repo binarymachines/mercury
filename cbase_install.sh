e05f288add9e365c30c209d500f4e0b823c8b7eb
Dexter Taylor <binarymachineshop@gmail.com>
2017-07-23 23:51:13 +0000

    all scripts in /scripts directory

--- cbase_install.sh
+++
@@ -1,6 +0,0 @@
-# Only needed during first-time setup:
-wget http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-2-amd64.deb
-sudo dpkg -i couchbase-release-1.0-2-amd64.deb
-sudo apt-get update
-sudo apt-get install libcouchbase-dev build-essential python-dev python-pip
-sudo pip install couchbase
