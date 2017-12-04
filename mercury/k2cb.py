829c192a37f9969999346acbc83f420387116e72
Dexter Taylor <binarymachineshop@gmail.com>
2017-12-04 16:22:00 -0500

    separate out couchbase-related code from telegraf

---
+++ mercury/k2cb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+
+from telegraf import DataRelay
+import couchbasedbx as cbx
+
+
+
+class CouchbaseRelay(DataRelay):
+    def __init__(self, host, bucket, record_type, keygen_function, **kwargs):
+        DataRelay.__init__(self, **kwargs)
+        self._record_type = record_type
+        couchbase_server = cbx.CouchbaseServer(host)
+        self._couchbase_mgr = cbx.CouchbasePersistenceManager(couchbase_server, bucket)
+        self._couchbase_mgr.register_keygen_function(self._record_type, keygen_function)
+
+
+    def _send(self, src_message_header, message_data, logger):
+        builder = cbx.CouchbaseRecordBuilder(self._record_type)
+        builder.from_json(message_data)
+        cb_record = builder.build()
+        key = self._couchbase_mgr.insert_record(cb_record)
+        logger.info('new record key: %s' % key)
+
+
--- mercury/telegraf.py
+++ mercury/telegraf.py
@@ -11,11 +11,6 @@ import yaml
 from snap import common
 import sqldbx as sqlx
 
-try:
-    import couchbasedbx as cbx
-except:
-    pass
-
 import logging
 import copy
 from kafka import KafkaProducer, KafkaConsumer, KafkaClient
@@ -436,25 +431,6 @@ class ConsoleRelay(DataRelay):
 
     def _send(self, src_message_header, message_data, logger):
         print '### record at offset %d: %s' % (src_message_header.offset, message_data)
-
-
-
-
-class CouchbaseRelay(DataRelay):
-    def __init__(self, host, bucket, record_type, keygen_function, **kwargs):
-        DataRelay.__init__(self, **kwargs)
-        self._record_type = record_type
-        couchbase_server = cbx.CouchbaseServer(host)
-        self._couchbase_mgr = cbx.CouchbasePersistenceManager(couchbase_server, bucket)
-        self._couchbase_mgr.register_keygen_function(self._record_type, keygen_function)
-
-
-    def _send(self, src_message_header, message_data, logger):
-        builder = cbx.CouchbaseRecordBuilder(self._record_type)
-        builder.from_json(message_data)
-        cb_record = builder.build()
-        key = self._couchbase_mgr.insert_record(cb_record)
-        logger.info('new record key: %s' % key)
 
 
 
