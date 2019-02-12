978a97c479c5872403074998ce4eec22931dda2f
Dexter Taylor <binarymachineshop@gmail.com>
2019-02-11 17:21:03 -0800

    bugfixes

--- mercury/datamap.py
+++ mercury/datamap.py
@@ -596,6 +596,7 @@ class RecordTransformerBuilder(object):
             default_transform_func = getattr(datasource, default_transform_funcname)
             transformer.set_default_transform(default_transform_func)
 
+        '''
         output_fields = []
         if not hasattr(self._transform_config['maps'][self._map_name], 'fields'):
             transformer.set_csv_output_header(output_fields)
@@ -606,6 +607,7 @@ class RecordTransformerBuilder(object):
                 output_fields.append(key)
 
         transformer.set_csv_output_header(output_fields)
+        '''
 
         for field_config in self._transform_config['maps'][self._map_name]['fields']:
             for fieldname, field_config in field_config.items():
@@ -642,13 +644,13 @@ class RecordTransformerBuilder(object):
                 else:
                     raise Exception('unrecognized source type "%s." Allowed types are record, lookup, and value.' % field_config['source'])                
 
-        '''
+        
         output_fields = []
         for field_config in self._transform_config['maps'][self._map_name]['fields']:
             for key, value in field_config.items():
                 output_fields.append(key)
         transformer.set_csv_output_header(output_fields)
-        '''
+        
         return transformer
 
 
